8aad3af7fcd5abf2c26ace3bedaed58edee24378
ZackMurray89 <zackmurray.dev@gmail.com>
2024-07-16 11:43:10 -0500

    create getposts api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,9 +1,9 @@
 import bcryptjs from 'bcryptjs'
-import User from '../models/user.model.js'
 import { errorHandler } from '../utils/error.js'
+import User from '../models/user.model.js'
 
 export const test = (req, res) => {
-  res.json({ message: 'API Is Working' })
+  res.json({ message: 'API is working!' })
 }
 
 export const updateUser = async (req, res, next) => {
@@ -24,6 +24,9 @@ export const updateUser = async (req, res, next) => {
     }
     if (req.body.username.includes(' ')) {
       return next(errorHandler(400, 'Username cannot contain spaces'))
+    }
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorHandler(400, 'Username must be lowercase'))
     }
     if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
       return next(
@@ -52,12 +55,12 @@ export const updateUser = async (req, res, next) => {
 }
 
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.userId) {
-    return next(errorHandler(403, 'You are not allowed to delete this account'))
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to delete this user'))
   }
   try {
     await User.findByIdAndDelete(req.params.userId)
-    res.status(200).json({ message: 'User deleted successfully' })
+    res.status(200).json('User has been deleted')
   } catch (error) {
     next(error)
   }
